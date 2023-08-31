a2a72cd47ec556882af027275e1939ac9232fb99
Dušan Simić <dusan.simic1810@gmail.com>
2023-08-31 02:45:50 +0200

    Brisanje nepotrebnog zakomentarisanog koda

--- Julia/02_components/twitch_exp.jl
+++ Julia/02_components/twitch_exp.jl
@@ -46,24 +46,9 @@ println("$(now()): Calculating Pearson correlations...")
 correlations = cor([core_num deg betw clos], dims=1)
 save_object("/out/twitch_pearson_correlation.jld2", correlations)
 
-# pearson_heatmap = heatmap(["core number", "degree", "betweenness", "closeness"], ["core number", "degree", "betweenness", "closeness"], correlations, c=:blues)
-# Plots.svg(pearson_heatmap, "/out/twitch_pearson_correlation.svg")
-
-# plot_shell_deg = scatter(core_num, deg, xlabel="core number", ylabel="degree", legend=false)
-# plot_shell_betw = scatter(core_num, betw, xlabel="core number", ylabel="betweenness", legend=false)
-# plot_shell_clos = scatter(core_num, clos, xlabel="core number", ylabel="closeness", legend=false)
-
-# correlation_scatter = plot(plot_shell_deg, plot_shell_betw, plot_shell_clos, layout=(1, 3), size=(1200, 400))
-
-# Plots.svg(correlation_scatter, "/out/twitch_correlation_scatter")
-
 println("$(now()): Calculating Spearman correlations...")
 spearman_correlation = Iterators.product([core_num, deg, betw, clos], [core_num, deg, betw, clos]) |> collect |> m -> map(x -> corspearman(x[1], x[2]), m)
 save_object("/out/twitch_spearman_correlation.jld2", spearman_correlation)
-
-# spearman_heatmap = heatmap(["core number", "degree", "betweenness", "closeness"], ["core number", "degree", "betweenness", "closeness"], spearman_correlation, c=:blues)
-
-# Plots.svg(spearman_heatmap, "/out/twitch_spearman_correlation.svg")
 
 function macroscopic_characteristics_analysis(g::SimpleGraph)
   G = SimpleGraph(nv(g))
@@ -129,29 +114,3 @@ end
 println("$(now()): Calculating characteristics...")
 characteristics = macroscopic_characteristics_analysis(twitch_graph)
 save_object("/out/twitch_characteristics.jld2", characteristics)
-
-# ks = map(i -> convert(Int64, i), characteristics[1, :])
-
-# nums_nodes = map(i -> convert(Int64, i), characteristics[2, :])
-# nums_edges = map(i -> convert(Int64, i), characteristics[3, :])
-# graph_densities = characteristics[4, :]
-# nums_connected_components = map(i -> convert(Int64, i), characteristics[5, :])
-# percentages_of_nodes_in_gcc = characteristics[6, :]
-# percentages_of_edges_in_gcc = characteristics[7, :]
-# smallworldnesses = characteristics[8, :]
-# diameters = map(i -> convert(Int64, i), characteristics[9, :])
-# clustering_coefficients = characteristics[10, :]
-
-# plot_shell_num_nodes = scatter(ks, nums_nodes, xlabel="k", ylabel="number of nodes", legend=false)
-# plot_shell_num_edges = scatter(ks, nums_edges, xlabel="k", ylabel="number of edges", legend=false)
-# plot_shell_graph_density = scatter(ks, graph_densities, xlabel="k", ylabel="graph density", legend=false)
-# plot_shell_num_connected_components = scatter(ks, nums_connected_components, xlabel="k", ylabel="number of connected components", legend=false)
-# plot_shell_percentage_of_nodes_in_gcc = scatter(ks, percentages_of_nodes_in_gcc, xlabel="k", ylabel="percentage of nodes in gcc", legend=false)
-# plot_shell_percentage_of_edges_in_gcc = scatter(ks, percentages_of_edges_in_gcc, xlabel="k", ylabel="percentage of edges in gcc", legend=false)
-# plot_shell_smallworldness = scatter(ks, smallworldnesses, xlabel="k", ylabel="smallworldness", legend=false)
-# plot_shell_diameter = scatter(ks, diameters, xlabel="k", ylabel="diameter", legend=false)
-# plot_shell_clustering_coefficient = scatter(ks, clustering_coefficients, xlabel="k", ylabel="clustering coefficient", legend=false)
-
-# characteristics_plots = plot(plot_shell_num_nodes, plot_shell_num_edges, plot_shell_graph_density, plot_shell_num_connected_components, plot_shell_percentage_of_nodes_in_gcc, plot_shell_percentage_of_edges_in_gcc, plot_shell_smallworldness, plot_shell_diameter, plot_shell_clustering_coefficient, layout=(3, 3), size=(1200, 800))
-
-# Plots.svg(characteristics_plots, "/out/twitch_characteristics.svg")
